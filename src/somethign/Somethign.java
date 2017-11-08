ccb72aad103422ef98c7c8d5f6fc2cee3357b3d8
B1 <B1@B149>
2017-11-08 13:13:50 +0800

    [Deliver #152266668]

--- src/somethign/Somethign.java
+++ src/somethign/Somethign.java
@@ -21,7 +21,7 @@ public class Somethign {
         for(int i =0;i<5;i++)
             System.out.println("Perhaps");
         
-       System.out.println("something");
+       System.out.println("something1");
     }
     
 }
